51f236a3089c2dccc468ece743ba08b955db6e69
unknown <G00387867@gmit.ie>
2020-02-13 20:42:13 +0000

    Added Task3 and variables

---
+++ Task3.py
@@ -0,0 +1,9 @@
+# program asks a user to input a string and outputs 
+# every second letter in reverse order
+
+
+sentence = str(input("please Enter a Sentence: "))
+
+reverseSentence = sentence[ : :-1]
+
+print(reverseSentence[ : :2])
---
+++ variables.py
@@ -0,0 +1,28 @@
+# practising task 3
+
+x = 911.0
+y = 2
+
+z = x + y
+
+
+print(z)
+
+fac7 = 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+print(fac7)
+
+b = True
+c = False
+
+print(c)
+
+s = "g00387867@gmit.ie"
+
+print(s[0:9:2])
+
+l = ['a', 'b','c', 4, 5, 6, 7, 8, 9]
+
+print(l[2:6:3])
+
+
